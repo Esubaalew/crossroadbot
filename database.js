5193ac9eb0b8b4cfd7b85365855c6a91e5cc43d1
Esubaalew <esubalewchekol6@gmail.com>
2024-02-21 15:53:11 +0300

    moved models code into databse.js code

--- database.js
+++ database.js
@@ -1,11 +1,8 @@
 // main definition of sequelize
 
-const {Sequelize} = require('sequelize');
+const {Sequelize, DataTypes} = require('sequelize');
 const sequelize = new Sequelize('sqlite://./data.sqlite');
 
-// Getting tables from models
-const User = require('models/User');
-const Admin = require('models/Admin');
 
 // testing if the connection is succeeded
 function checkConnection() {
@@ -19,5 +16,48 @@ function checkConnection() {
         });
 }
 
+const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true
+        },
+        role: {
+            type: DataTypes.STRING,
+            defaultValue: 'user'
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    },
+    {tableName: "Users"}
+)
+
+const Admin = sequelize.define('Admin', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: true
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'admin'
+        },
+    },
+    {tableName: "Admins"}
+);
 
 module.exports = {sequelize, User, Admin};
